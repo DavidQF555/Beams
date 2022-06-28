450ea0ea6c6b37fe9b643e2bdb8b762558a0d900
DavidQF555 <davidfeng2003@gmail.com>
2022-06-28 05:36:59 -0700

    Improved Fire Module

--- src/main/java/io/github/davidqf555/minecraft/beams/common/modules/FireModuleType.java
+++ src/main/java/io/github/davidqf555/minecraft/beams/common/modules/FireModuleType.java
@@ -1,7 +1,11 @@
 package io.github.davidqf555.minecraft.beams.common.modules;
 
 import io.github.davidqf555.minecraft.beams.common.entities.BeamEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.FireBlock;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
 
 public class FireModuleType extends ProjectorModuleType {
 
@@ -14,5 +18,15 @@ public class FireModuleType extends ProjectorModuleType {
     @Override
     public void onEntityTick(BeamEntity beam, Entity target) {
         target.setSecondsOnFire(duration);
+    }
+
+    @Override
+    public void onBlockTick(BeamEntity beam, BlockPos pos) {
+        if (beam.level.isEmptyBlock(pos)) {
+            BlockState fire = Blocks.FIRE.defaultBlockState();
+            if (((FireBlock) Blocks.FIRE).canSurvive(fire, beam.level, pos)) {
+                beam.level.setBlockAndUpdate(pos, fire);
+            }
+        }
     }
 }
