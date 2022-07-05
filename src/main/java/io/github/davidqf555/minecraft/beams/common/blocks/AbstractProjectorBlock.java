cd3231e3cd61edca7b1d96a6b33848d6969e84b9
DavidQF555 <davidfeng2003@gmail.com>
2022-07-05 14:09:43 -0700

    Ported to 1.18.2

--- src/main/java/io/github/davidqf555/minecraft/beams/common/blocks/AbstractProjectorBlock.java
+++ src/main/java/io/github/davidqf555/minecraft/beams/common/blocks/AbstractProjectorBlock.java
@@ -1,29 +1,28 @@
 package io.github.davidqf555.minecraft.beams.common.blocks;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ContainerBlock;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.ItemStack;
-import net.minecraft.state.BooleanProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.state.properties.BlockStateProperties;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.world.World;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.Containers;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.Vec3;
 
 import javax.annotation.Nullable;
 
-public abstract class AbstractProjectorBlock extends ContainerBlock {
+public abstract class AbstractProjectorBlock extends BaseEntityBlock {
 
     public static final BooleanProperty TRIGGERED = BlockStateProperties.TRIGGERED;
 
@@ -33,26 +32,26 @@ public abstract class AbstractProjectorBlock extends ContainerBlock {
 
     @SuppressWarnings("deprecation")
     @Override
-    public ActionResultType use(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult clip) {
+    public InteractionResult use(BlockState state, Level world, BlockPos pos, Player player, InteractionHand hand, BlockHitResult clip) {
         if (world.isClientSide()) {
-            return ActionResultType.SUCCESS;
+            return InteractionResult.SUCCESS;
         } else {
-            TileEntity te = world.getBlockEntity(pos);
+            BlockEntity te = world.getBlockEntity(pos);
             if (te instanceof ProjectorTileEntity) {
                 player.openMenu((ProjectorTileEntity) te);
             }
-            return ActionResultType.CONSUME;
+            return InteractionResult.CONSUME;
         }
     }
 
     @SuppressWarnings("deprecation")
     @Override
-    public void neighborChanged(BlockState state, World world, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean update) {
+    public void neighborChanged(BlockState state, Level world, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean update) {
         super.neighborChanged(state, world, pos, neighborBlock, neighborPos, update);
         if (!world.isClientSide()) {
             boolean triggered = state.getValue(TRIGGERED);
             if (triggered != world.hasNeighborSignal(pos)) {
-                TileEntity te = world.getBlockEntity(pos);
+                BlockEntity te = world.getBlockEntity(pos);
                 if (te instanceof ProjectorTileEntity) {
                     world.setBlock(pos, state.cycle(TRIGGERED), 2);
                     if (triggered) {
@@ -67,27 +66,27 @@ public abstract class AbstractProjectorBlock extends ContainerBlock {
 
     @SuppressWarnings("deprecation")
     @Override
-    public void onPlace(BlockState state, World world, BlockPos pos, BlockState old, boolean update) {
+    public void onPlace(BlockState state, Level world, BlockPos pos, BlockState old, boolean update) {
         if (state.getValue(TRIGGERED)) {
-            TileEntity te = world.getBlockEntity(pos);
+            BlockEntity te = world.getBlockEntity(pos);
             if (te instanceof ProjectorTileEntity) {
                 ((ProjectorTileEntity) te).updateBeams();
             }
         }
     }
 
-    protected abstract Vector3d getStartOffset(BlockState state);
+    protected abstract Vec3 getStartOffset(BlockState state);
 
-    protected abstract Vector3d getBeamDirection(BlockState state);
+    protected abstract Vec3 getBeamDirection(BlockState state);
 
     @SuppressWarnings("deprecation")
     @Override
-    public void onRemove(BlockState state1, World world, BlockPos pos, BlockState state2, boolean update) {
+    public void onRemove(BlockState state1, Level world, BlockPos pos, BlockState state2, boolean update) {
         if (!state1.is(state2.getBlock())) {
-            TileEntity te = world.getBlockEntity(pos);
+            BlockEntity te = world.getBlockEntity(pos);
             if (te instanceof ProjectorTileEntity) {
                 ((ProjectorTileEntity) te).removeBeams();
-                InventoryHelper.dropContents(world, pos, (ProjectorTileEntity) te);
+                Containers.dropContents(world, pos, (ProjectorTileEntity) te);
                 world.updateNeighbourForOutputSignal(pos, this);
             }
         }
@@ -95,9 +94,9 @@ public abstract class AbstractProjectorBlock extends ContainerBlock {
     }
 
     @Override
-    public void setPlacedBy(World world, BlockPos pos, BlockState state, LivingEntity entity, ItemStack stack) {
+    public void setPlacedBy(Level world, BlockPos pos, BlockState state, LivingEntity entity, ItemStack stack) {
         if (stack.hasCustomHoverName()) {
-            TileEntity te = world.getBlockEntity(pos);
+            BlockEntity te = world.getBlockEntity(pos);
             if (te instanceof ProjectorTileEntity) {
                 ((ProjectorTileEntity) te).setCustomName(stack.getHoverName());
             }
@@ -105,24 +104,24 @@ public abstract class AbstractProjectorBlock extends ContainerBlock {
     }
 
     @Override
-    public BlockRenderType getRenderShape(BlockState state) {
-        return BlockRenderType.MODEL;
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.MODEL;
     }
 
     @Nullable
     @Override
-    public TileEntity newBlockEntity(IBlockReader reader) {
-        return new ProjectorTileEntity();
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return new ProjectorTileEntity(pos, state);
     }
 
     @Override
-    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(TRIGGERED);
     }
 
     @Nullable
     @Override
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
         BlockState prev = super.getStateForPlacement(context);
         if (prev == null) {
             return null;
