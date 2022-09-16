9abf11e30ad88b57e8231ef619af33129a4734b5
DavidQF555 <54233412+DavidQF555@users.noreply.github.com>
2022-09-16 02:34:49 -0500

    Improved Force Module Item

--- src/main/java/io/github/davidqf555/minecraft/beams/common/modules/ForceModuleType.java
+++ src/main/java/io/github/davidqf555/minecraft/beams/common/modules/ForceModuleType.java
@@ -8,19 +8,18 @@ import java.util.function.Function;
 
 public class ForceModuleType extends ProjectorModuleType {
 
-    private final int period;
     private final Function<Integer, Double> magnitude;
 
-    public ForceModuleType(int period, Function<Integer, Double> magnitude) {
+    public ForceModuleType(Function<Integer, Double> magnitude) {
         this.magnitude = magnitude;
-        this.period = period;
     }
 
     @Override
     public void onEntityTick(BeamEntity beam, Entity target, int amt) {
-        if (target.level.getGameTime() % period == 0 && target.isPushable()) {
+        if (target.isPushable()) {
             Vector3d force = beam.position().subtract(beam.getStart()).normalize().scale(magnitude.apply(amt));
             target.push(force.x(), force.y(), force.z());
+            target.hurtMarked = true;
         }
     }
 }
