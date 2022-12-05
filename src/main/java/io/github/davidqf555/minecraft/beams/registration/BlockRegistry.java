ee235fc1d2ca98763c1a708b8bbe5d8ca4120b22
DavidQF555 <54233412+DavidQF555@users.noreply.github.com>
2022-12-04 22:13:45 -0600

    Fixed Mirror Block Strength

--- src/main/java/io/github/davidqf555/minecraft/beams/registration/BlockRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/beams/registration/BlockRegistry.java
@@ -9,6 +9,7 @@ import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,7 +23,7 @@ public final class BlockRegistry {
     public static final RegistryObject<SimpleProjectorBlock> PROJECTOR = register("projector", () -> new SimpleProjectorBlock(AbstractBlock.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3.5f)));
     public static final RegistryObject<TiltedProjectorBlock> TILTED_PROJECTOR = register("tilted_projector", () -> new TiltedProjectorBlock(AbstractBlock.Properties.of(Material.STONE).requiresCorrectToolForDrops().strength(3.5f)));
     public static final RegistryObject<OmnidirectionalProjectorBlock> OMNIDIRECTIONAL_PROJECTOR = register("omnidirectional_projector", () -> new OmnidirectionalProjectorBlock(AbstractBlock.Properties.of(Material.GLASS).sound(SoundType.GLASS).noOcclusion().isSuffocating((state, reader, pos) -> false).isViewBlocking((state, reader, pos) -> false).strength(0.3f)));
-    public static final RegistryObject<MirrorBlock> MIRROR = register("mirror", () -> new MirrorBlock(AbstractBlock.Properties.of(Material.METAL).strength(3.5f)));
+    public static final RegistryObject<MirrorBlock> MIRROR = register("mirror", () -> new MirrorBlock(AbstractBlock.Properties.of(Material.METAL, MaterialColor.GOLD).requiresCorrectToolForDrops().strength(3, 6).sound(SoundType.METAL)));
 
     private BlockRegistry() {
     }
