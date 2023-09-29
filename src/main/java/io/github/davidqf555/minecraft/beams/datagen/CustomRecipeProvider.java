2f9dfa0aefcee2444e646d62fec17a5055a56b4f
DavidQF555 <davidfeng2003@gmail.com>
2023-09-29 03:24:52 -0500

    ported

--- src/main/java/io/github/davidqf555/minecraft/beams/datagen/CustomRecipeProvider.java
+++ src/main/java/io/github/davidqf555/minecraft/beams/datagen/CustomRecipeProvider.java
@@ -2,14 +2,12 @@ package io.github.davidqf555.minecraft.beams.datagen;
 
 import io.github.davidqf555.minecraft.beams.registration.ItemRegistry;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.DyeItem;
-
-import java.util.function.Consumer;
 
 public class CustomRecipeProvider extends RecipeProvider {
 
@@ -18,7 +16,7 @@ public class CustomRecipeProvider extends RecipeProvider {
     }
 
     @Override
-    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
+    protected void buildRecipes(RecipeOutput consumer) {
         for (DyeColor color : ItemRegistry.COLOR_MODULES.keySet()) {
             ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ItemRegistry.COLOR_MODULES.get(color)::get).requires(ItemRegistry.BLANK_MODULE::get).requires(DyeItem.byColor(color)).unlockedBy("has_module", has(ItemRegistry.BLANK_MODULE::get)).save(consumer);
         }
