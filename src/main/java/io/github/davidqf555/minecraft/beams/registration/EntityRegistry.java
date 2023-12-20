4c84662a7c49045bc3af3e5b732b3078be70dcb7
DavidQF555 <davidfeng2003@gmail.com>
2023-12-20 06:34:32 -0600

    removed outdated subscriber

--- src/main/java/io/github/davidqf555/minecraft/beams/registration/EntityRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/beams/registration/EntityRegistry.java
@@ -6,11 +6,9 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod.EventBusSubscriber(modid = Beams.ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class EntityRegistry {
 
     public static final DeferredRegister<EntityType<?>> TYPES = DeferredRegister.create(ForgeRegistries.ENTITIES, Beams.ID);
