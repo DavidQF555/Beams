3a98298b95be7dbea21e94cef4f72afb6147319d
DavidQF555 <davidfeng2003@gmail.com>
2024-02-26 05:10:18 -0600

    fixed omnidirectional projector

--- src/main/java/io/github/davidqf555/minecraft/beams/registration/TileEntityRegistry.java
+++ src/main/java/io/github/davidqf555/minecraft/beams/registration/TileEntityRegistry.java
@@ -15,17 +15,18 @@ public final class TileEntityRegistry {
     public static final DeferredRegister<BlockEntityType<?>> TYPES = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, Beams.ID);
 
     public static final RegistryObject<BlockEntityType<ContainerProjectorTileEntity>> BEAM_PROJECTOR = register("beam_projector", () -> BlockEntityType.Builder.of(ContainerProjectorTileEntity::new, BlockRegistry.PROJECTOR.get(), BlockRegistry.TILTED_PROJECTOR.get()).build(null));
-    public static final RegistryObject<BlockEntityType<OmnidirectionalProjectorTileEntity>> OMNIDIRECTIONAL_BEAM_PROJECTOR = register("omnidirectional_beam_projector", () -> BlockEntityType.Builder.of(OmnidirectionalProjectorTileEntity::new, BlockRegistry.OMNIDIRECTIONAL_PROJECTOR.get()).build(null));
+    public static final RegistryObject<BlockEntityType<PointableProjectorTileEntity>> OMNIDIRECTIONAL_BEAM_PROJECTOR = register("omnidirectional_beam_projector", () -> BlockEntityType.Builder.of(PointableProjectorTileEntity::new, BlockRegistry.OMNIDIRECTIONAL_PROJECTOR.get()).build(null));
     public static final RegistryObject<BlockEntityType<RedirectorTileEntity>> MIRROR = register("mirror", () -> BlockEntityType.Builder.of(RedirectorTileEntity::new, BlockRegistry.MIRROR.get()).build(null));
     public static final RegistryObject<BlockEntityType<BeamSensorTileEntity>> BEAM_SENSOR = register("beam_sensor", () -> BlockEntityType.Builder.of(BeamSensorTileEntity::new, BlockRegistry.PHOTODETECTOR.get()).build(null));
-    private TileEntityRegistry() {
-    }    public static final RegistryObject<BlockEntityType<PointableRedirectorTileEntity>> OMNIDIRECTIONAL_MIRROR = register("omnidirectional_mirror", () -> BlockEntityType.Builder.of(PointableRedirectorTileEntity::new, BlockRegistry.OMNIDIRECTIONAL_MIRROR.get()).build(null));
+
+    public static final RegistryObject<BlockEntityType<PointableRedirectorTileEntity>> OMNIDIRECTIONAL_MIRROR = register("omnidirectional_mirror", () -> BlockEntityType.Builder.of(PointableRedirectorTileEntity::new, BlockRegistry.OMNIDIRECTIONAL_MIRROR.get()).build(null));
     public static final RegistryObject<BlockEntityType<TurretTileEntity>> TURRET = register("turret", () -> BlockEntityType.Builder.of(TurretTileEntity::new, BlockRegistry.TURRET.get()).build(null));
 
     private static <T extends BlockEntity> RegistryObject<BlockEntityType<T>> register(String name, Supplier<BlockEntityType<T>> type) {
         return TYPES.register(name, type);
     }
 
-
+    private TileEntityRegistry() {
+    }
 
 }
